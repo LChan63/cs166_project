c9279fcdea6b55fd5a2d602bb4be18e9a4469b8e
Shinjin Santhakumar <shinjinoliver@gmail.com>
2021-05-18 12:38:59 -0700

    Update Group_19_Luccas_Shinjin_schema.sql
    added basic attributes plus the has relationship between doctor and appointment

--- phase2/Group_19_Luccas_Shinjin_schema.sql
+++ phase2/Group_19_Luccas_Shinjin_schema.sql
@@ -4,3 +4,48 @@ drop table appointment;
 drop table staff;
 drop table doctor;
 drop table department;
+
+CREATE TABLE hospital(
+	hospital_id NUMERIC(9,0) PRIMARY KEY,
+	hname name(30),
+    
+);
+
+CREATE TABLE patient(
+	patient_id NUMERIC(9,0) PRIMARY KEY,
+	pname name(30),
+    pgender char(1),
+    page NUMERIC(5,0),
+    paddress CHAR(40),
+    num_appointments NUMERIC(9,0),
+
+);
+
+CREATE TABLE appointment(
+	appnt_id NUMERIC(9,0) PRIMARY KEY,
+	timeslot NUMERIC(3,0),
+    adate DATE,
+
+);
+
+CREATE TABLE doctor(
+	doctor_id NUMERIC(9,0) PRIMARY KEY,
+	dname char(30),
+    speciality CHAR(25),
+
+);
+
+CREATE TABLE staff(
+    stuff_id NUMERIC(9,0) PRIMARY KEY,
+    sname char(30),
+
+);
+
+--relationship between appointment and doctor many to many
+CREATE TABLE has(
+    appnt_id NUMERIC(9,0),
+    doctor_id NUMERIC(3,0),
+    PRIMARY KEY(appnt_id, doctor_id),
+    FOREIGN KEY (appnt_id) REFERENCES appointment(appnt_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id)
+);
