042501f11257c30551b25701d17fe4db6aaf2615
Shinjin Santhakumar <shinjinoliver@gmail.com>
2021-05-19 17:13:51 -0700

    Update Group_19_Luccas_Shinjin_schema.sql
    updating NOT NULL

--- phase2/Group_19_Luccas_Shinjin_schema.sql
+++ phase2/Group_19_Luccas_Shinjin_schema.sql
@@ -18,66 +18,66 @@ drop table schedule cascade;
 drop table search cascade;
 
 CREATE TABLE hospital(
-	hospital_id NUMERIC(9,0) PRIMARY KEY,
+	hospital_id NUMERIC(9,0) PRIMARY KEY NOT NULL,
 	hname char(30)    
 );
 
 CREATE TABLE patient(
-	patient_id NUMERIC(9,0) PRIMARY KEY,
+	patient_id NUMERIC(9,0) PRIMARY KEY NOT NULL,
 	pname char(30),
-    pgender char(1),
-    page NUMERIC(5,0),
-    paddress CHAR(40),
-    num_appointments NUMERIC(9,0)
+    	pgender char(1),
+    	page NUMERIC(5,0),
+    	paddress CHAR(40),
+    	num_appointments NUMERIC(9,0)
 );
 
 CREATE TABLE appointment(
-	appnt_id NUMERIC(9,0) PRIMARY KEY,
+	appnt_id NUMERIC(9,0) PRIMARY KEY NOT NULL,
 	timeslot NUMERIC(3,0),
-    adate DATE
+    	adate DATE
 );
 
 create table past_appnt (
-	appnt_id numeric(9,0),
+	appnt_id numeric(9,0) NOT NULL,
 	primary key (appnt_id),
 	foreign key (appnt_id) references appointment(appnt_id)
 );
 
 create table active_appnt (
-        appnt_id numeric(9,0),
+        appnt_id numeric(9,0) NOT NULL,
         primary key (appnt_id),
         foreign key (appnt_id) references appointment(appnt_id)
 );
 
 create table available_appnt (
-        appnt_id numeric(9,0),
+        appnt_id numeric(9,0) NOT NULL,
         primary key (appnt_id),
         foreign key (appnt_id) references appointment(appnt_id)
 );
 
 create table waitlist_appnt (
-        appnt_id numeric(9,0),
+        appnt_id numeric(9,0) NOT NULL,
         primary key (appnt_id),
         foreign key (appnt_id) references appointment(appnt_id)
 );
 
 
 CREATE TABLE staff(
-    staff_id NUMERIC(9,0) PRIMARY KEY,
-    sname char(30),
+    	staff_id NUMERIC(9,0) PRIMARY KEY NOT NULL,
+    	sname char(30),
 	hospital_id NUMERIC(9,0),
 	foreign key (hospital_id) references hospital(hospital_id) 
 );
 
 create table department (
-	dept_id numeric(9,0) primary key,
+	dept_id numeric(9,0) primary key NOT NULL,
 	name char(20),
 	part_of numeric(9,0),
 	foreign key (part_of) references hospital(hospital_id)
 );
 
 CREATE TABLE doctor(
-        doctor_id NUMERIC(9,0) PRIMARY KEY,
+        doctor_id NUMERIC(9,0) PRIMARY KEY NOT NULL,
         dname char(30),
     speciality CHAR(25),
         work_dept numeric(9,0),
@@ -87,16 +87,16 @@ CREATE TABLE doctor(
 
 --relationship between appointment and doctor many to many
 CREATE TABLE has(
-    appnt_id NUMERIC(9,0),
-    doctor_id NUMERIC(3,0),
-    PRIMARY KEY(appnt_id, doctor_id),
-    FOREIGN KEY (appnt_id) REFERENCES appointment(appnt_id),
-    FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id)
+    	appnt_id NUMERIC(9,0) NOT NULL,
+    	doctor_id NUMERIC(3,0) NOT NULL,
+    	PRIMARY KEY(appnt_id, doctor_id),
+    	FOREIGN KEY (appnt_id) REFERENCES appointment(appnt_id),
+    	FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id)
 );
 
 create table request_maintenance (
-	doctor_id NUMERIC(9,0),
-	staff_id NUMERIC(9,0),
+	doctor_id NUMERIC(9,0) NOT NULL,
+	staff_id NUMERIC(9,0) NOT NULL,
 	patient_per_hour integer,
 	dept_name char(20),
 	time_slot timestamp,
@@ -106,17 +106,17 @@ create table request_maintenance (
 );
 
 create table schedule (
-	staff_id numeric(9,0),
-	appnt_id numeric(9,0),
+	staff_id numeric(9,0) NOT NULL,
+	appnt_id numeric(9,0) NOT NULL,
 	primary key (staff_id, appnt_id),
 	foreign key (staff_id) references staff(staff_id),
 	foreign key (appnt_id) references appointment(appnt_id)
 );
 
 create table search (
-	hospital_id numeric(9,0),
-	patient_id numeric(9,0),
-	appnt_id numeric(9,0),
+	hospital_id numeric(9,0) NOT NULL,
+	patient_id numeric(9,0) NOT NULL,
+	appnt_id numeric(9,0) NOT NULL,
 	primary key (hospital_id, patient_id, appnt_id),
 	foreign key (hospital_id) references hospital(hospital_id),
 	foreign key (patient_id) references patient(patient_id),
